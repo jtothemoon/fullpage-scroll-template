19be525f65f8f52972b43570a1084a80528ddc1b
Jtothemoon <jtothemoon@naver.com>
2025-09-04 14:00:16 +0900

    패드 스크롤 힌트 수정

--- src/components/ScrollHint.tsx
+++ src/components/ScrollHint.tsx
@@ -16,15 +16,15 @@ const ScrollHint = ({ isVisible, className = '' }: ScrollHintProps) => {
 
   return (
     <div className={`scroll-hint ${className}`}>
-      {/* 데스크톱 버전 */}
-      <div className="hidden md:flex flex-col items-center justify-center text-gray-600 dark:text-gray-300 animate-bounce">
+      {/* 데스크톱 버전 (마우스 연결) */}
+      <div className="hidden lg:flex flex-col items-center justify-center text-gray-600 dark:text-gray-300 animate-bounce">
         <Mouse className="h-6 w-6 mb-2" />
         <p className="text-sm font-medium">{t('scroll.explore')}</p>
         <ChevronDown className="h-4 w-4 mt-1" />
       </div>
 
-      {/* 모바일 버전 */}
-      <div className="md:hidden flex flex-col items-center justify-center text-gray-600 dark:text-gray-300 animate-bounce">
+      {/* 패드/모바일 버전 (터치/제스처) */}
+      <div className="lg:hidden flex flex-col items-center justify-center text-gray-600 dark:text-gray-300 animate-bounce">
         <div className="flex flex-col items-center">
           <Pointer className="h-6 w-6 mb-2" />
           <p className="text-xs font-medium">{t('scroll.swipe_up')}</p>
